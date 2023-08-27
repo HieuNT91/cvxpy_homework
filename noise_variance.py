c5b2dd1cc056e1fa92f68d5516671b8498ff3b93
HieuNT91 <hilljun.2000@gmail.com>
2023-08-27 01:59:01 +0000

    add vectorised variance solver

---
+++ noise_variance.py
@@ -0,0 +1,80 @@
+import numpy as np
+import cvxpy as cp
+from matplotlib import pyplot as plt
+from sklearn.metrics import pairwise_distances
+
+from memory_profiler import profile 
+from functools import wraps
+from time import time
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func:%r took: %2.4f sec' % \
+          (f.__name__, te-ts))
+        return result
+    return wrap
+
+# @profile
+@timing
+def test(pairwise_dists, verbose=True):
+    N = pairwise_dists.shape[0]
+    sigmas = cp.Variable(N)
+
+    constraints = [sigmas >= 1e-8]
+    for i in range(N-1):
+        for j in range(i+1, N):
+            constraints.append(sigmas[i] + sigmas[j] >= pairwise_dists[i, j])
+
+    # obj = 0
+    # for i in range(N):
+    #     obj += sigmas[i] ** 2 
+    obj = cp.Minimize(cp.sum_squares(sigmas))
+    prob = cp.Problem(obj, constraints)
+    print("Solving with MOSEK...")
+    prob.solve('MOSEK', verbose=verbose)
+    print("L=", pairwise_dists)
+    print("sigma=", sigmas.value)
+    return sigmas.value
+
+
+@timing
+def test_vectorised(pairwise_dists, verbose=True):
+    N = pairwise_dists.shape[0]
+    sigmas = cp.Variable(N)
+
+    constraints = [sigmas >= 1e-8]
+    for i in range(N-1):
+        constraints.append(sigmas[i] + sigmas[i+1:] >= pairwise_dists[i, i+1:])
+    #     for j in range(i+1, N):
+    #         constraints.append(sigmas[i] + sigmas[j] >= pairwise_dists[i, j])
+    
+
+    # obj = 0
+    # for i in range(N):
+    #     obj += sigmas[i] ** 2 
+    obj = cp.Minimize(cp.sum_squares(sigmas))
+    prob = cp.Problem(obj, constraints)
+    print("Solving with MOSEK...")
+    prob.solve('MOSEK', verbose=verbose)
+    print("L=", pairwise_dists)
+    print("sigma=", sigmas.value)
+    return sigmas.value
+
+
+if __name__ == "__main__":
+
+    np.random.seed(3)
+    # N = 50000
+    # d = 32*32*3
+    Z = np.load('small_noises.npy').reshape(200, -1)
+    # Z = Z[:200, :20]
+    # np.save('small_noises.npy', Z)
+    weight = 1
+    pairwise_dists = weight * pairwise_distances(Z)
+    print('Shape of pairwise_dists:', pairwise_dists.shape)
+    V = test_vectorised(pairwise_dists)
+    
